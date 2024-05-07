5e6cab315e0aa376a02a36e7ecb6422dcd345ea5
Ahmed alaa <aalaa@instabug.com>
2024-05-07 17:45:32 +0300

    ci: fix ci pub dev

--- release.sh
+++ release.sh
@@ -3,10 +3,12 @@ VERSION=$(egrep -o "version: ([0-9]-*.*)+[0-9]" pubspec.yaml | cut -d ":" -f 2)
 if [ ! "${VERSION}" ] || [ -z "${VERSION}" ];then
     echo "Instabug: err: Version Number not found."
     exit 1
-else 
+else
+  print(${PUB_CREDENTIALS})
     mkdir -p .pub-cache
-    cat <<EOF > $HOME/.pub-cache/credentials.json
+    cat <<EOF > $HOME/.config/dart/pub-credentials.json
     ${PUB_CREDENTIALS}
+
 EOF
     flutter packages pub publish -f
 fi
